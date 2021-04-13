172336b8635f357c67f0d5f58e1d05a1c442c07e
camiloMartinezRincon <camilo.martinez1989@gmail.com>
2021-04-12 21:46:41 -0500

    formularios y validators version 1

--- src/app/paginas/autentificacion/iniciar-sesion/iniciar-sesion.page.ts
+++ src/app/paginas/autentificacion/iniciar-sesion/iniciar-sesion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciar-sesion.page.scss'],
 })
 export class IniciarSesionPage implements OnInit {
+  logForm: FormGroup;
+  isSubmited = false;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.logForm = this.formBuilder.group({
+      logEmail: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      logPassword: ['', [Validators.required,]]
+    });
   }
 
+  submitLogForm() {
+    this.isSubmited = true;
+    if (!this.logForm.valid) {
+      console.log("Por favor llenar todos los campos");
+      return false;
+    } else {
+      console.log(this.logForm.value);
+    }
+    
+  }
 }
--- src/app/paginas/autentificacion/registro/registro.page.ts
+++ src/app/paginas/autentificacion/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  registerForm: FormGroup;
+  isSubmited = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      regName: ['', [Validators.required]],
+      regLastName: ['', [Validators.required]],
+      regEmail: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      logPassword: ['', [Validators.required, Validators.minLength(8)]],
+      logRePassword: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+  submitRegisterForm() {
+    this.isSubmited = true;
+    if (!this.registerForm.valid) {
+      console.log("Por favor llenar todos los campor requeridos");
+    } else {
+      console.log(this.registerForm.value);
+    }
   }
 
+  confirmPassword() {
+
+  }
 }
